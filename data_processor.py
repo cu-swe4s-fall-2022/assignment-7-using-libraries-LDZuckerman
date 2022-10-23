1604512a4797bc45c2da1863e76a6d59fcb36fa5
LeahDZuckerman <LeahDZuckerman@gmail.com>
2022-10-23 16:44:37 -0600

    add functionality to pass test

--- data_processor.py
+++ data_processor.py
@@ -1,7 +1,11 @@
-# remember to import your libraries!
+import numpy as np
+import pandas as pd
 
 def get_random_matrix(num_rows, num_columns):
-	return None
+
+	a = np.random.rand(num_rows, num_columns)
+	
+	return a
 
 def get_file_dimensions(file_name):
 	return (0,0)
