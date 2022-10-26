51155d60d0120de865a72daa8e526ad06db4a60b
LeahDZuckerman <LeahDZuckerman@gmail.com>
2022-10-26 17:41:25 -0600

    change to allow user to input if csv has header in get dimensions function

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,7 +2,7 @@ import unittest
 import random
 import numpy as np
 import sys
-sys.path.append('../')
+#sys.path.append('../')
 import data_processor  # nopep8
 
 
@@ -13,9 +13,9 @@ class TestUtils(unittest.TestCase):
 
         """Create toy data for tests
         """
-        array_5_6 = a = np.empty((5,6))
-        np.savetxt("test56.csv", array_5_6, delimiter=",")
-        cls.testfile_5_6 = 'test56.csv'
+        array56 = a = np.empty((5,6))
+        np.savetxt("test56.csv", array56, delimiter=",")
+        cls.f56 = 'test56.csv'
 
     @classmethod
     def tearDownClass(cls):
@@ -29,14 +29,22 @@ class TestUtils(unittest.TestCase):
         """
 
         # positive test: returns data of correct shape
-        cls.assertEqual(np.shape(data_processor.get_random_matrix(2, 3)), (2, 3))
+        cls.assertEqual(np.shape(data_processor.get_random_matrix(2, 3)),
+                        (2, 3))
 
         # negative test: does not return data of incorrect shape
-        cls.assertNotEqual(np.shape(data_processor.get_random_matrix(2, 3)), (3, 2))
+        cls.assertNotEqual(np.shape(data_processor.get_random_matrix(2, 3)),
+                           (3, 2))
 
         # check that correct errors are raised
-        cls.assertRaises(TypeError, data_processor.get_random_matrix, 'three', 3)
-        cls.assertRaises(ValueError, data_processor.get_random_matrix, -2, 3)
+        cls.assertRaises(TypeError,
+                         data_processor.get_random_matrix,
+                         'three',
+                         3)
+        cls.assertRaises(ValueError,
+                         data_processor.get_random_matrix,
+                         -2,
+                         3)
 
     def test_get_file_dimensions(cls):
 
@@ -44,13 +52,22 @@ class TestUtils(unittest.TestCase):
         """
         
         # positive test: returns correct shape
-        cls.assertEqual(data_processor.get_file_dimensions(cls.testfile_5_6), (5, 6))
+        cls.assertEqual(data_processor.get_file_dimensions(cls.f56, False),
+                        (5, 6))
 
         # negative test: does not return data of incorrect shape
-        cls.assertNotEqual(data_processor.get_file_dimensions(cls.testfile_5_6), (6, 5))
+        cls.assertNotEqual(data_processor.get_file_dimensions(cls.f56, False),
+                          (6, 5))
 
         # check that correct errors are raised
-        cls.assertRaises(FileNotFoundError, data_processor.get_file_dimensions, 'csvDNE.csv')
+        cls.assertRaises(ValueError,
+                         data_processor.get_file_dimensions,
+                         'csvDNE.csv',
+                         has_header='No')
+        cls.assertRaises(FileNotFoundError,
+                         data_processor.get_file_dimensions,
+                         'csvDNE.csv',
+                         has_header=False)
 
     def test_write_matrix_to_file(cls):
 
@@ -59,15 +76,29 @@ class TestUtils(unittest.TestCase):
         
         # positive test: test that matrix created has the correct dimensions 
         data_processor.write_matrix_to_file(2,3, 'testfile.csv')
-        cls.assertEqual(data_processor.get_file_dimensions('testfile.csv'), (2,3))
+        true_dim = data_processor.get_file_dimensions('testfile.csv',
+                                                      has_header=False)
+        cls.assertEqual(true_dim, (2,3))
 
         # negative test: 
-        cls.assertNotEqual(data_processor.get_file_dimensions('testfile.csv'), (3,2))
+        cls.assertNotEqual(true_dim, (3,2))
 
         # check that correct errors are raised
-        cls.assertRaises(TypeError, data_processor.write_matrix_to_file, 'three', 3, 'file.csv')
-        cls.assertRaises(ValueError, data_processor.write_matrix_to_file, -2, 3, 'file.csv')
-        cls.assertRaises(NameError, data_processor.write_matrix_to_file, 2, 3, 2)
+        cls.assertRaises(TypeError,
+                         data_processor.write_matrix_to_file,
+                         'three',
+                         3,
+                         'file.csv')
+        cls.assertRaises(ValueError,
+                         data_processor.write_matrix_to_file,
+                         -2,
+                         3,
+                         'file.csv')
+        cls.assertRaises(NameError,
+                         data_processor.write_matrix_to_file,
+                         2,
+                         3,
+                         2)
 
 if __name__ == '__main__':
     unittest.main()
