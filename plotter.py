cea45bee1696d96c128bf2e211cea33d16bdfd8f
LeahDZuckerman <LeahDZuckerman@gmail.com>
2022-10-25 21:38:22 -0600

    add argparsing for output file names

--- plotter.py
+++ plotter.py
@@ -1,8 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import argparse
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--boxplot_filename',
+                        type=str,
+                        help='[string] The name of the file to save the'
+                        + ' boxplot to',
+                        required=True)
+    parser.add_argument('--scatterplot_filename',
+                        type=str,
+                        help='[string] The name of the file to save the'
+                        + ' scatter plot to',
+                        required=True)
+    parser.add_argument('--comboplot_filename',
+                        type=str,
+                        help='[string] The name of the file to save the'
+                        + ' combination plot to',
+                        required=True)
+    args = parser.parse_args()
+
+    boxplot_filename = args.boxplot_filename 
+    scatterplot_filename = args.scatterplot_filename 
+    comboplot_filename = args.comboplot_filename 
 
     # get data
     iris = pd.read_csv('iris.data', header=None) 
@@ -12,7 +35,7 @@ def main():
     measurement_names = ['sepal_width','sepal_length', 'petal_width', 'petal_length']
     plt.boxplot(iris[measurement_names], labels=measurement_names)
     plt.ylabel('cm')
-    plt.savefig('iris_boxplot.png')
+    plt.savefig(boxplot_filename)
 
     # create petal_width_v_length_scatter.png
     plt.figure()
@@ -22,7 +45,7 @@ def main():
     plt.xlabel('sepal width (cm)')
     plt.ylabel('sepal length (cm)')
     plt.legend()
-    plt.savefig('petal_width_v_length_scatter.png')
+    plt.savefig(scatterplot_filename)
 
     # create multi_panel_figure.png
     fig, axs = plt.subplots(1,2, figsize=(10,5))
@@ -37,7 +60,7 @@ def main():
     axs[1].set_xlabel('sepal width (cm)')
     axs[1].set_ylabel('sepal length (cm)')
     plt.legend()
-    plt.savefig('multi_panel_figure.png')
+    plt.savefig(comboplot_filename)
 
 if __name__ == "__main__":
     main()
