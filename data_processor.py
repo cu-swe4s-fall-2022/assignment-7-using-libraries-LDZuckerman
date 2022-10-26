3e283a10ee63bbe65012d3f3cd7006f4b9a60398
LeahDZuckerman <LeahDZuckerman@gmail.com>
2022-10-25 21:24:35 -0600

    add docustrings

--- data_processor.py
+++ data_processor.py
@@ -3,6 +3,21 @@ import pandas as pd
 
 def get_random_matrix(num_rows, num_columns):
 
+	"""" Create a matrix of random numbers
+
+	Parameters:
+    -----------
+    num_rows: int
+		The number of rows in the matrix
+	num_cols: int
+		The number of collumns in the matrix
+
+    Returns:
+    -------
+    a
+        The random matrix
+	"""
+
 	try:
 		a = np.random.rand(num_rows, num_columns)
 	except TypeError:
@@ -15,14 +30,45 @@ def get_random_matrix(num_rows, num_columns):
 
 def get_file_dimensions(file_name):
 
+	"""" Find the dimensions of the array contained in a CSV file
+
+	Parameters:
+    -----------
+    file_name: string
+		The name of the file containing the array
+
+    Returns:
+    -------
+    shape
+        The shape of the array contained in the file
+	"""
+
 	try:
 		df = pd.read_csv(file_name, header=None)
 	except FileNotFoundError:
 		raise FileNotFoundError('File '+str(file_name)+' not found')
 
-	return np.shape(df)
+	shape = np.shape(df)
+
+	return shape
 
 def write_matrix_to_file(num_rows, num_columns, file_name):
+
+	"""" Write a random matrix to a CSV file
+
+	Parameters:
+    -----------
+    num_rows: int
+		The number of rows in the matrix
+	num_cols: int
+		The number of collumns in the matrix
+	file_name: string
+		The name of the file to save the array to 
+
+    Returns:
+    -------
+    Creates CSV file containing random data array
+	"""
 
 	if type(file_name) != str:
 		raise NameError('file_name must be a string')
