adf856ca115fe2452b009e1dce7e4318a5e52d62
LeahDZuckerman <LeahDZuckerman@gmail.com>
2022-10-26 16:34:04 -0600

    convert to unix from windows formatso it runs

--- tests/functional_tests.sh
+++ tests/functional_tests.sh
@@ -1,34 +1,34 @@
-#!/bin/bash
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
 # test plotter.py
-wget "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-run test_plotter.py python plotter.py
+run test_plotter.py python plotter.py \
+   --boxplot_filename='iris_boxplot.png' \
+   --scatterplot_filename='petal_width_v_length_scatter.png' \
+   --comboplot_filename='multi_panel_figure.png' \
 
 filepath='iris_boxplot.png'
 echo $filepath
 if [ -f "$filepath" ]; then
-   echo ' TEST 1 PASSED: found '+filename+' in expected directory'
+   echo ' TEST 1 PASSED: found '$filepath$' in expected directory'
 else
-   echo ' TEST 1 FAILED: did not find '+filename+' in expected directory'
+   echo ' TEST 1 FAILED: did not find '$filepath$' in expected directory'
 fi
-assert_exit_code 0
 
 filepath='petal_width_v_length_scatter.png'
 echo $filepath
 if [ -f "$filepath" ]; then
-   echo ' TEST 2 PASSED: found '+filename+' in expected directory'
+   echo ' TEST 2 PASSED: found '$filepath$' in expected directory'
 else
-   echo ' TEST 2 FAILED: did not find '+filename+' in expected directory'
+   echo ' TEST 2 FAILED: did not find '$filepath$' in expected directory'
 fi
-assert_exit_code 0
 
 filepath='multi_panel_figure.png'
 echo $filepath
 if [ -f "$filepath" ]; then
-   echo ' TEST 3 PASSED: found '+filename+' in expected directory'
+   echo ' TEST 3 PASSED: found '$filepath$' in expected directory'
 else
-   echo ' TEST 3 FAILED: did not find '+filename+' in expected directory'
+   echo ' TEST 3 FAILED: did not find '$filepath$' in expected directory'
 fi
+
 assert_exit_code 0
